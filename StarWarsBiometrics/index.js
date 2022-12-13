101a5be71efef61057978fcdc086e23d31ca48c3
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-12-13 15:35:59 +0100

    Clean-up

--- StarWarsBiometrics/index.js
+++ StarWarsBiometrics/index.js
@@ -1,14 +1,21 @@
 const fetchBiometrics = function (el) {
   const output = document.querySelector('textarea');
-  const uri = 'https://www.swapi.tech/api/people/?name=' + el[0].value;
+  const searchTerm = el[0].value;
+  const uri = 'https://www.swapi.tech/api/people/?name=' + searchTerm;
+  output.textContent = `Du sökte efter \"${searchTerm}\"`;
 
   fetch(uri)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      const { mass, height, gender, hair_color } = data.result[0].properties;
-      const text = `Vikt: ${mass}, längd: ${height}, kön: ${gender}, hårfärg: ${hair_color}`;
-      output.textContent = text;
+      if (data.result.length) {
+        data.result.forEach((element) => {
+          const { name, mass, height, gender, hair_color } = element.properties;
+          const text = `\n\nInfo om ${name}\n\nVikt: ${mass}, längd: ${height}, kön: ${gender}, hårfärg: ${hair_color}`;
+          output.textContent += text;
+        });
+      } else {
+        output.textContent += `\n\nInga resultat.`;
+      }
     })
     .catch((err) => console.log(err));
 };
